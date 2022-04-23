0d25e9d03de11afdaf143b610b41ceaf89fd5ffe
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-04-23 03:12:58 +0300

    fixed a processing error in negative numbers. file authoring added.

--- Python/Collatz-Calc.py
+++ Python/Collatz-Calc.py
@@ -1,21 +1,30 @@
-ct=0
+ct = 0
 
-inputNumber=float(input("Enter Number here: "))
+inputNumber = float(input("Enter Number here: "))
 
-print(ct,"=",inputNumber)
+print(ct, "=", inputNumber)
+
+of = open(str(inputNumber)+".txt", "a")
+of.write(str(ct)+" = "+(str(inputNumber)+'\n'))
 
 while(True):
-    
-    ct=ct+1
+    ct = ct+1
 
-    if(inputNumber==1 or inputNumber==-1):
+    if(inputNumber == 1 or inputNumber == -1):
         break
 
-    if(inputNumber%2==0):
-        inputNumber=(inputNumber/2)
+    elif(inputNumber % 2 == 0):
+        inputNumber = (inputNumber/2)
+
+    elif(inputNumber < 0):
+        inputNumber = (inputNumber*3)-1
 
     else:
-        inputNumber=(inputNumber*3)+1
+        inputNumber = (inputNumber*3)+1
 
-    print(ct,"=",inputNumber)
+    of.write(str(ct)+" = "+(str(inputNumber)+'\n'))
+    print(ct, "=", inputNumber)
+
+of.flush()
+of.close()
 exit()
